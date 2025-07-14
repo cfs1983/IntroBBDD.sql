89a5ed939fa7caf596a7674c6d7331001be0e0c7
cfs1983 <cfs.1983@gmail.com>
2025-07-14 18:34:21 +0200

    1

---
+++ IntroBBDD.sql
@@ -0,0 +1,12 @@
+SELECT flight_id, flight_on, status FROM flights
+WHERE status = "on time";
+
+SELECT * FROM bookings WHERE total_amount >= 1000000;
+
+SELECT * FROM aircrafts_data;
+
+SELECT flight_id, flight_on, FROM flights 
+WHERE aircrafts_code = "733";
+
+SELECT * FROM tikets 
+WHERE passenger_name = "IRINA%";
